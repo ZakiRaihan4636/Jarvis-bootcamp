50cafac78e13fa8f6aefe1fd34f89024c9087787
Muhamad Zaki Raihan <55277007+ZakiRaihan4636@users.noreply.github.com>
2022-05-05 11:47:15 +0700

    Tugas Praktikum 8 PHP OOP

--- pertemuan 8/index.php
+++ pertemuan 8/index.php
@@ -6,23 +6,23 @@ require_once 'Ape.php';
 
 $sheep = new Animal("shaun");
 
-echo "Name : " . $sheep->name . "\n";
-echo "Legs : " . $sheep->legs . "\n";
-echo "cold blooded : " . $sheep->cold_blooded . "\n" . "\n";
+echo "Name : " . $sheep->name . "<br>";
+echo "Legs : " . $sheep->legs . "<br>";
+echo "cold blooded : " . $sheep->cold_blooded . "<br><br>";
 
 
-$kodok = new Frog("buduk");
+$kodok = new Frog("Buduk");
 
-echo "Name : " . $kodok->name . "\n";
-echo "Legs : " . $kodok->legs . "\n";
-echo "cold blooded : " . $kodok->cold_blooded . "\n";
-echo $kodok->jump() . "\n\n";
+echo "Name : " . $kodok->name . "<br>";
+echo "Legs : " . $kodok->legs . "<br>";
+echo "cold blooded : " . $kodok->cold_blooded . "<br>";
+echo $kodok->jump() . "<br><br>";
 
 
 
 $sungokong = new Ape("kera sakti");
 
-echo "Name : " . $sungokong->name . "\n";
-echo "Legs : " . $sungokong->legs . "\n";
-echo "cold blooded : " . $sungokong->cold_blooded . "\n";
+echo "Name : " . $sungokong->name . "<br>";
+echo "Legs : " . $sungokong->legs . "<br>";
+echo "cold blooded : " . $sungokong->cold_blooded . "<br>";
 echo $sungokong->yell();

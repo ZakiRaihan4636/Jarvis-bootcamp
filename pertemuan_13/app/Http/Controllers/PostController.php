011e09e85b83701e028112f331edf1a51cc9be67
Muhamad Zaki Raihan <toteyoku4636@gmail.com>
2022-05-29 03:03:38 +0700

    Tugas pertemuan 13 CRUD Laravel

---
+++ pertemuan_13/app/Http/Controllers/PostController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    //
+    public function index()
+    {
+        $result = Post::all();
+        return view('index', ['posts' => $result]);
+    }
+
+    public function create()
+    {
+        return view('create');
+    }
+
+    public function store(Request $request)
+    {
+        $data = [
+            "title" => $request->title,
+            "body" => $request->body
+        ];
+
+
+        $create = Post::create($data);
+
+        if ($create) {
+            return redirect('/');
+        }
+    }
+
+    public function update($id, Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+
+        $post = Post::Find($id);
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->update();
+        return redirect('/');
+    }
+
+
+    public function edit($id)
+    {
+        $result = Post::Find($id);
+
+        return view('edit', ['post' => $result]);
+    }
+
+    public function show($id)
+    {
+        $result = Post::Find($id);
+        return view('detail', ['post' => $result]);
+    }
+
+    public function destroy($id)
+    {
+        Post::destroy($id);
+
+        return redirect('/');
+    }
+}
